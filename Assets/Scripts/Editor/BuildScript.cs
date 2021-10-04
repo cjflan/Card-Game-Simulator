dd9fa3a6e523c3f342dc2f23cd0686052b70d6f9
connorjf <connorjf@gatech.edu>
2021-10-04 14:21:31 -0400

    buildscript

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -1,19 +1,45 @@
-using UnityEditor;
+// using UnityEditor;
 
-public class BuildScript 
+// public class BuildScript 
+// {
+//     [MenuItem("Custom Utilities/Build StandaloneLinux64")]
+//     static void PerformBuild()
+//     {
+//         string[] scenes = { "Assets/Scenes/CardsExplorer.unity", "Assets/Scenes/DeckExplored.unity", "Assets/Scenes/MainMenu.unity", "Assets/Scenes/PlayGame.unity", "Assets/Scenes/Settings.unity", "Assets/Scenes/TitleScreen.unity" };
+//         BuildPipeline.BuildPlayer(scesnes, "../../../build/Card-Game-Simulator.app",
+//             BuildTarget.StandaloneOSX, BuildOptions.None);
+//     }
+// }
+
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+
+// Output the build size or a failure depending on BuildPlayer.
+
+public class BuildScript : MonoBehaviour
 {
-    [MenuItem("Custom Utilities/Build StandaloneLinux64")]
-    static void PerformBuild()
+    [MenuItem("Build/Build OSX")]
+    public static void PreformBuild()
     {
         string[] scenes = { "Assets/Scenes/CardsExplorer.unity", "Assets/Scenes/DeckExplored.unity", "Assets/Scenes/MainMenu.unity", "Assets/Scenes/PlayGame.unity", "Assets/Scenes/Settings.unity", "Assets/Scenes/TitleScreen.unity" };
-        BuildPipeline.BuildPlayer(scesnes, "../../../build/Card-Game-Simulator.app",
-            BuildTarget.StandaloneOSX, BuildOptions.None);
-    }
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = scenes;
+        buildPlayerOptions.locationPathName = "../../../OSXbuild";
+        buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+        buildPlayerOptions.options = BuildOptions.None;
 
-    [MenuItem("Custom Utilities/Build Asset Bundle StandaloneLinux64")]
-    static void PerformAssetBundleBuild()
-    {
-        BuildPipeline.BuildAssetBundles("../AssetBundles/", BuildAssetBundleOptions.ChunkBasedCompression,
-            BuildTarget.StandaloneOSX);
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
     }
 }
